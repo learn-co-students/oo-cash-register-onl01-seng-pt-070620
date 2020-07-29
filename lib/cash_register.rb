cc059870e0cc8612ba53e469663be47a33aa1261
Andy Adams <andrewdcadams@gmail.com>
2020-07-29 13:15:36 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,8 @@
 
 class CashRegister
-  
 
-  attr_accessor :total, :discount, :items, :lastitem,
+  attr_accessor :total, :discount, :items, :lastitem
 
-  
   def initialize(discount=0)
     @items = []
     @discount = discount
@@ -22,7 +20,6 @@ class CashRegister
       @items << title
       counter -= 1 
     end
-    @items[title] = price
     @total += (price * quantity)
     @lastitem = (price * quantity)
   end
@@ -32,31 +29,19 @@ class CashRegister
       return "There is no discount to apply."
     else
       otherDiscount = @discount.to_f
-
       @total = (@total * (1- (otherDiscount/100))).to_i
-    
       return "After the discount, the total comes to $#{@total}."
-      @total = (@total * (1- (otherDiscount/100)))
-    
-      return "success message"
     end
   end
   
   def items
-    @itemArray
+    @items
   end
   
   def void_last_transaction
-
-    @total -= @items[@lastitem]
-    @items.delete(@lastitem)
+    @total -= @lastitem
+    @items.pop
     @total = 0.0 if @items.empty?
-    @items.keys
   end
   
-  def void_last_transaction
-    total -= @@items.values.last 
-    @items.last.delete
-    @total = 0.0 if @@items.last == nil 
-  end
 end     #ends class 
