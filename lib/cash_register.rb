371ed6c4d864ed743190d3aabc782ead535f9a51
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-01 16:58:18 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
 
+require 'pry'
+class CashRegister
+  
+  attr_accessor :discount, :total, :add_item, :cash_register
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price)
+    binding.pry
+    price.collect |amt| 
+    
+      price + @total
+    end
+  end
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+end
