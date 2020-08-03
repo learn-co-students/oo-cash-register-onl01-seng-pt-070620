9479480fc4021583b25ff931265d28d173f628da
Jill Short <jillian.b.short@gmail.com>
2020-08-03 22:33:21 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
-
+require "pry"
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(discount = 20)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, quantity = 1, price)
+    @total = price * quantity + @total
+  end
+  
+  def apply_discount
+    @total = @total - (@total * 0.2) 
+    return "After the discount, the total comes to $#{@total.round}."
+  end
+  
+  def items(title, quantity)
+    items.collect do |item|
+    end
+  end
+  
+end
