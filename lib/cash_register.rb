dc7eb1bf7eddfee6d049171d9a445e80fd2a2f9f
Jesse Murphy <jessemurphy1982@gmail.com>
2020-08-07 02:35:53 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,44 +9,30 @@ class CashRegister
   end
   
   def add_item(item, price, quantity = 1)
-    item_info = {}
-    item_info[:name] = item 
-    item_info[:price] = price
-    item_info[:quantity] = quantity
-    
-    @items << item_info
-    
-    @total += price * quantity
-    @last_transaction_amount = total
-    @total
-  end 
-  
+   if quantity > 1 
+     i = 0 
+     while i< quantity
+     @items << item 
+     i += 1 
+   end 
+ else
+   @items << item 
+ end
+ @total += price * quantity
+ @last_transaction_amount = @total
+ @total 
+ end 
+
   def apply_discount 
-    if discount == 0 
-      return "There is no discount to apply."
+     if discount == 0 
+    return "There is no discount to apply."
     end 
-    
-    @total -= @total * @discount / 100 
+     @total -= @total * @discount / 100
     return "After the discount, the total comes to $#{@total}."
   end 
-  
-  def items
-   item_name = []
-    @items.each do |item_info|
-      for qty in 1..item_info[:quantity]
-      item_name << item_info [:name]
-    end
-  end
-  item_name
-end 
-
+  binding.pry 
   def void_last_transaction()
-    @total -= @last_transaction_amount
-    
-   # binding.pry 
+    @total = @last_transaction_amount
   end
   
-  
-
-
 end 
