4419a7a9bee7e407d0ebc4640ad25a7df63afbd3
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-08-07 02:43:33 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,10 @@ class CashRegister
     @total = 0
     @discount = discount
     @all_items = []
+<<<<<<< HEAD
     @last_trans_amt = 0
+=======
+>>>>>>> 394695ae18b07a6fdb4f22341b64dba6219178c6
   end
   
   def add_item(title, price, quantity = 1)
@@ -17,15 +20,23 @@ class CashRegister
     @total += price
     if quantity == 1
       @all_items << title
+<<<<<<< HEAD
       @last_trans_amt = price * quantity
+=======
+>>>>>>> 394695ae18b07a6fdb4f22341b64dba6219178c6
     elsif quantity > 1
       quantity.times do 
         @all_items << title
       end
+<<<<<<< HEAD
       @last_trans_amt = price
     else
       @last_trans_amt = 0
     end
+=======
+    end
+    # @last_trans_amt = price * quantity
+>>>>>>> 394695ae18b07a6fdb4f22341b64dba6219178c6
   end
   
   def apply_discount
@@ -43,7 +54,18 @@ class CashRegister
   end
   
   def void_last_transaction
+<<<<<<< HEAD
     @total = @total - @last_trans_amt
     @last_trans_amt
+=======
+    self.total -= @last_trans_amt
+    # binding.pry
+    # binding.pry
+    # if @all_items.size == 0
+    #   @total = 0.0
+    # end
+    # @total -= @last_trans_amt
+    # @total
+>>>>>>> 394695ae18b07a6fdb4f22341b64dba6219178c6
   end
 end
